312d04b59c2e824ab4c777ef0876c10f72cf3c48
ali <atemurtas97@gmail.com>
2025-02-20 16:00:25 +0300

    aligorithm-exercise

---
+++ exercise/exercise.py
@@ -0,0 +1,61 @@
+# Important python usages
+
+
+def build_hash_map(keys: List[str], values: List[int]) -> Dict[str, int]:
+    output_dict = {}
+
+    for key, value in zip(keys, values):
+        output_dict[key] = value
+      
+    return output_dict
+
+
+
+my_dict = {"a": 1, "b": 2, "c": 3}
+
+for key, value in my_dict.items():
+    print(key, value)
+
+
+
+
+nums = [1, 2, 4, 3, 2, 1]
+freq = {}
+
+for num in nums:
+    if num in freq:
+        freq[num] += 1
+    else:
+        freq[num] = 1
+
+print(freq)  # {1: 2, 2: 2, 4: 1, 3: 1}
+
+
+
+from collections import defaultdict
+
+nums = [1, 2, 4, 3, 2, 1]
+freq = defaultdict(int)
+
+for num in nums:
+    freq[num] += 1
+
+print(freq)  # {1: 2, 2: 2, 4: 1, 3: 1}
+
+
+def count_chars(s: str) -> Dict[str, int]:
+    count = defaultdict(int)
+    for char in s:
+        count[char] += 1
+    return count
+
+
+
+
+def nested_list_to_dict(nums: List[List[int]]) -> Dict[int, List[int]]:
+    output = defaultdict(list)
+    for sublist in nums:
+        first = sublist[0]
+        for i in range(1,len(sublist)):
+            output[first].append(sublist[i])
+    return output
