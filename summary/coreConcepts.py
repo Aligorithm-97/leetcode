411001c0891a67d5a9791d7bc659a6694b447474
Aligorithm-97 <atemurtas97@gmail.com>
2025-02-21 22:12:00 +0300

    aligortihm-core

---
+++ summary/coreConcepts.py
@@ -0,0 +1,21 @@
+"""
+
+Abstract Class : A class that can contain abstract methods and cannot be instantiated directly. It can have both abstract and regular methods.
+
+Interface : A structure that contains only method signatures. It supports multiple inheritance.
+
+Polymorphism : Allows the same method to behave differently. It is divided into Overriding and Overloading.
+
+Overloading : Methods defined in the same class with the same name but different parameters.
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+                                        DSA Neetcode
+
+Static arr : The problem with static array they have fixed size. Python offers dynamic arrays as default.
+
+Linked List : Consist of nodes -> value and next -> head and tail
+
+Doubly Linked List : extra prev node
+
+"""
