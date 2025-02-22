ea201b4cb51e48d2061092cf4e9119782eb85428
Aligorithm-97 <atemurtas97@gmail.com>
2025-02-22 10:39:37 +0300

    aligorithm-core

--- summary/coreConcepts.py
+++ summary/coreConcepts.py
@@ -18,4 +18,21 @@ Linked List : Consist of nodes -> value and next -> head and tail
 
 Doubly Linked List : extra prev node
 
+Recurcion : Factorial , Fibonacci number : F(n) = F(n-1) + F(n-2) -> F(0) = 0, F(1) = 1
+
+    Sorting
+
+Insertion Sort : every time compare two element in order and swap if it is necessary. it is an iterative solution so it includes loops --> o(n'2)
+
+Merge Sort : divide and conquer -> everytime divide the array to the half of it and sort from the smallest one --> o(nlogn)
+
+Quick Sort : pick a pivot compare numbers with it if it is less then put it into left container etc. --> o(nlogn)
+
+Bucket Sort : if we have small range of members like 0 to 2 -> find the element occurrences and put it in a bucket.
+
+Binary Search : You can use it only if it is sorted. Higher or lower half.
+
+Binary Search Tree : Sorted
+
+
 """
