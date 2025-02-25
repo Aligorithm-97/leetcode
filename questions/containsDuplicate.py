950c243f39e7411649c63826562c31463999c11d
ali <atemurtas97@gmail.com>
2025-02-25 13:16:25 +0300

    aligorithm-extraSolutions

--- questions/containsDuplicate.py
+++ questions/containsDuplicate.py
@@ -17,3 +17,26 @@ solution = Solution()
 x = solution.hasDuplicate([1,2,3,4])
 
 print(x)
+
+"""
+
+Solution with Sorting
+time : o(nlogn)
+space : o(n)
+
+"""
+
+class SolutionTwo:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+        
+
+solutionTwo = SolutionTwo()
+
+x = solutionTwo.hasDuplicate([1,2,1,4])
+
+print(x)
--- questions/groupAnagrams.py
+++ questions/groupAnagrams.py
@@ -1,6 +1,15 @@
 from collections import defaultdict
 from typing import List
 
+
+"""
+
+Solution with Sorting
+time : o(m * nlogn)
+space : o(m * n)
+
+"""
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
@@ -8,3 +17,32 @@ class Solution:
             sortedS = ''.join(sorted(s))
             res[sortedS].append(s)
         return list(res.values())
+    
+strs = ["act","pots","tops","cat","stop","hat"]
+sol = Solution()
+print(sol.groupAnagrams(strs))
+    
+
+
+"""
+
+Solution with Hash Table
+time : o(m * n)
+space : o(m)
+
+"""
+
+class SolutionTwo:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s :
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+    
+
+strs = ["act","pots","tops","cat","stop","hat"]
+solTwo = SolutionTwo()
+print(solTwo.groupAnagrams(strs))
