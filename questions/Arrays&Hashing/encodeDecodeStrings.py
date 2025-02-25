2d29d130acc064559cd3b9276b18dd6478c98245
ali <atemurtas97@gmail.com>
2025-02-25 16:06:13 +0300

    aligorithm-encodeDecodeStrings

---
+++ questions/Arrays&Hashing/encodeDecodeStrings.py
@@ -0,0 +1,38 @@
+from typing import List
+
+"""
+
+Solution Optimal
+time : o(n)
+space : o(1)
+
+"""
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+            
+        return res
+    
+
+sol = Solution()
+print(sol.encode(["a","code","b","you"]))
+print(sol.decode(sol.encode(["a","code","b","you"])))
