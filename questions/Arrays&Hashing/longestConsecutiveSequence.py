91e03d378f7fece2a03821ff600726978c38a6a5
ali <atemurtas97@gmail.com>
2025-02-26 10:17:12 +0300

    aligorithm-longestConsecutive

--- questions/Arrays&Hashing/longestConsecutiveSequence.py
+++ questions/Arrays&Hashing/longestConsecutiveSequence.py
@@ -1,5 +1,13 @@
+from collections import defaultdict
 from typing import List
 
+"""
+
+Solution with Hash Set
+time : O(n)
+space : o(n)
+
+"""
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
@@ -17,3 +25,29 @@ class Solution:
 sol = Solution()
 nums = [2,20,4,10,3,4,6]
 print(sol.longestConsecutive(nums))
+
+
+"""
+
+Solution with Hash Set
+time : O(n)
+space : o(n)
+
+"""
+
+
+class SolutionTwo:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+        res = 0
+        for num in nums:
+            if not mp[num]:
+                mp[num] = mp[num-1]+ mp[num+1] + 1
+                mp[num - mp[num-1]] = mp[num]
+                mp[num + mp[num+1]] = mp[num]
+                res = max(res, mp[num])
+        return res
+    
+solTwo = SolutionTwo()
+nums = [2,20,4,10,3,4,5]
+print(solTwo.longestConsecutive(nums))
