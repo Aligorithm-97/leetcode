2c386245d9d3c964548c97111c38d4ab9a361b2f
ali <atemurtas97@gmail.com>
2025-02-27 14:37:21 +0300

    aligorithm-twoSum2withExplanation

--- questions/TwoPointers/twoSum2.py
+++ questions/TwoPointers/twoSum2.py
@@ -1,6 +1,19 @@
 from typing import List
 
 
+"""
+
+Solution with Two Pointers
+time : O(n)
+space : o(1)
+
+Solution Explanation:
+    We place two pointers: one at the beginning and one at the end.
+        If the sum is greater than the target, we decrease the right pointer by one.
+        If the sum is less than the target, we increase the left pointer by one until we find the target.
+
+"""
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         l,r = 0,len(numbers) - 1
