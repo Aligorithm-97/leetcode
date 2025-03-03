4d08b0ddba8b1aea9460bb7f6d92f2008dcc6b22
ali <atemurtas97@gmail.com>
2025-03-03 11:34:14 +0300

    aligorithm-findMinumumInRotatedSorted

---
+++ questions/BinarySearch/findMinumumInRotatedSorted.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] < nums[r]:
+                r = m
+            else:
+                l = m + 1
+        return nums[l]
+    
+
+sol = Solution()
+nums = [3,4,5,6,1,2]
+print(sol.findMin(nums))
