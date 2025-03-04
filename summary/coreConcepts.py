88f2be6bcb8c06052d65abbb2bc5442205f85b88
ali <atemurtas97@gmail.com>
2025-03-04 10:11:58 +0300

    aligorithm-rearrangement

--- summary/coreConcepts.py
+++ summary/coreConcepts.py
@@ -1,16 +1,6 @@
 """
                                 DSA CheatSheet
 
-                OOP
-
-    Abstract Class : A class that can contain abstract methods and cannot be instantiated directly. It can have both abstract and regular methods.
-
-    Interface : A structure that contains only method signatures. It supports multiple inheritance.
-
-    Polymorphism : Allows the same method to behave differently. It is divided into Overriding and Overloading.
-
-    Overloading : Methods defined in the same class with the same name but different parameters.
-
 ----------------------------------------------------------------------------------------------------------------------------------------------
 
                 System Design
@@ -209,42 +199,19 @@
         And also since it is a huge traffic and involves all parts of the world we should use CDNs
         LRU cache can be implemented
 
+// TODO System Design and oop 
 
+                OOP
+
+    Abstract Class : A class that can contain abstract methods and cannot be instantiated directly. It can have both abstract and regular methods.
+
+    Interface : A structure that contains only method signatures. It supports multiple inheritance.
+
+    Polymorphism : Allows the same method to behave differently. It is divided into Overriding and Overloading.
+
+    Overloading : Methods defined in the same class with the same name but different parameters.
 
     
-// TODO add here
-
-
-
-- Sliding Window
-- Two Pointers
-- Prefix Sums
-- Fast and Slow Pointers
-
-
-
-Static arr : The problem with static array they have fixed size. Python offers dynamic arrays as default.
-
-Linked List : Consist of nodes -> value and next -> head and tail
-
-Doubly Linked List : extra prev node
-
-Recurcion : Factorial , Fibonacci number : F(n) = F(n-1) + F(n-2) -> F(0) = 0, F(1) = 1
-
-    Sorting
-
-Insertion Sort : every time compare two element in order and swap if it is necessary. it is an iterative solution so it includes loops --> o(n'2)
-
-Merge Sort : divide and conquer -> everytime divide the array to the half of it and sort from the smallest one --> o(nlogn)
-
-Quick Sort : pick a pivot compare numbers with it if it is less then put it into left container etc. --> o(nlogn)
-
-Bucket Sort : if we have small range of members like 0 to 2 -> find the element occurrences and put it in a bucket.
-
-Binary Search : You can use it only if it is sorted. Higher or lower half.
-
-Binary Search Tree : Sorted
-
 LinkedList
 
 Singly linkedList
