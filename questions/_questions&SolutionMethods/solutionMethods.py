9d7ff739c0603ad87cea0c317d6acf5936e6f38d
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-19 14:18:25 +0300

    aligorithm-242

--- questions/_questions&SolutionMethods/solutionMethods.py
+++ questions/_questions&SolutionMethods/solutionMethods.py
@@ -2,6 +2,8 @@
 
 LeetCode Problem 217 - Contains Duplicate : Create a set with given numbers and compare their lengths.
 
+LeetCode Problem 242 - Valid Anagram : Can use sorted() function and compare them but better solution is using hash table. Use ord('a') etc.
+
 
 
 """
