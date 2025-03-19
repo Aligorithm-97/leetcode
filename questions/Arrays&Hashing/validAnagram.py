988ec62d7c9e43a41ae179bb98c5c7718c6e3994
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-19 14:23:13 +0300

    aligorithm-validAnagramComment

--- questions/Arrays&Hashing/validAnagram.py
+++ questions/Arrays&Hashing/validAnagram.py
@@ -13,11 +13,11 @@ class Solution:
 
         count = [0] * 26
         for i in range(len(s)):
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
+            count[ord(s[i]) - ord('a')] += 1 # consider s[i] is a so count[0] = 1
+            count[ord(t[i]) - ord('a')] -= 1 # and then in this step it will be 0 if it is an anagram
 
         for val in count:
-            if val != 0:
+            if val != 0: # if there is a non 0 value then it is not an anagram
                 return False
         return True
 
