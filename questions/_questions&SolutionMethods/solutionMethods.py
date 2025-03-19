6c36a9501e524c4187d158d774a557e2d514dadf
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-19 15:18:34 +0300

    aligorithm-1

--- questions/_questions&SolutionMethods/solutionMethods.py
+++ questions/_questions&SolutionMethods/solutionMethods.py
@@ -4,6 +4,8 @@ LeetCode Problem 217 - Contains Duplicate : Create a set with given numbers and 
 
 LeetCode Problem 242 - Valid Anagram : Can use sorted() function and compare them but better solution is using hash table. Use ord('a') etc.
 
+LeetCode Problem 1 - Two Sum : Create a map and use enumerate() function with for loop and if diff value is in map simply return pmap[diff] , i.
+
 
 
 """
