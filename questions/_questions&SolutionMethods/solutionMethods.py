b23daecb17b368d7ec9d935d8577cc967187a21c
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-19 15:44:55 +0300

    aligorithm-49

--- questions/_questions&SolutionMethods/solutionMethods.py
+++ questions/_questions&SolutionMethods/solutionMethods.py
@@ -6,6 +6,7 @@ LeetCode Problem 242 - Valid Anagram : Can use sorted() function and compare the
 
 LeetCode Problem 1 - Two Sum : Create a map and use enumerate() function with for loop and if diff value is in map simply return pmap[diff] , i.
 
+LeetCode Problem 49 - Group Anagrams : Create a default dictionary and sort them in a for loop and use join function to generate also string because sorted() function returns an array. Then append every same sorted value into dictionary with their original values. Finally return list(res.values())
 
 
 """
