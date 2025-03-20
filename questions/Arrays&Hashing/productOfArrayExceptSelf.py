83176cbed465f6d2ef4c592dc0b8f41b8eb1a536
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-20 15:49:20 +0300

    aligorithm-prefixSuffix

--- questions/Arrays&Hashing/productOfArrayExceptSelf.py
+++ questions/Arrays&Hashing/productOfArrayExceptSelf.py
@@ -26,3 +26,28 @@ class Solution:
     
 sol = Solution()
 print(sol.productExceptSelf([-1,0,1,2,3]))
+
+"""
+
+Solution with Prefix & Suffix
+time : O(n)
+space : o(1)
+
+"""
+
+class SolutionTwo:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        print(res)
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+    
+solTwo = SolutionTwo()
+print(solTwo.productExceptSelf([-1,0,1,2,3]))
