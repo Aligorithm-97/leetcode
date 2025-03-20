7b0189f0b2006cc9b0033b8457cf50a684738bc4
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-20 10:20:31 +0300

    aligorithm-update

--- questions/_questions&SolutionMethods/solutionMethods.py
+++ questions/_questions&SolutionMethods/solutionMethods.py
@@ -1,20 +1,20 @@
 """
 
-LeetCode Problem 217 - Contains Duplicate : Create a set with given numbers and compare their lengths.
+1 = LeetCode Problem 217 - Contains Duplicate : Create a set with given numbers and compare their lengths.
 
-LeetCode Problem 242 - Valid Anagram : Can use sorted() function and compare them but better solution is using hash table. Use ord('a') etc.
+2 = LeetCode Problem 242 - Valid Anagram : Can use sorted() function and compare them but better solution is using hash table. Use ord('a') etc.
 
-LeetCode Problem 1 - Two Sum : Create a map and use enumerate() function with for loop and if diff value is in map simply return pmap[diff] , i.
+3 = LeetCode Problem 1 - Two Sum : Create a map and use enumerate() function with for loop and if diff value is in map simply return pmap[diff] , i.
 
-LeetCode Problem 49 - Group Anagrams : 
+4 = LeetCode Problem 49 - Group Anagrams : 
     1 - Sorting : Create a default dictionary and sort them in a for loop and use join function to generate also string because sorted() function returns an array. Then append every same sorted value into dictionary with their original values. Finally return list(res.values())
     2 - Hash Table : To efficiently group anagrams, we use a hash table where the key is a tuple representing letter frequencies. Each word is processed by counting occurrences of letters in a fixed-size array (26 elements), ensuring anagrams share the same key. Since tuples are immutable and hashable, they serve as unique identifiers for anagram groups.
 
-LeetCode Problem 347 - Top K Frequent Elements : 
+5 = LeetCode Problem 347 - Top K Frequent Elements : 
     1 - Sorting : Create a frequency table and group it with the values. Then sort it. Pop k times.
     2 - Heap : Use heap to get rid of sorting phase. Pop until k elements remains. Append remainings to res.
     3 - Bucket Sort : Create a bucket -> freq = [[] for i in range(len(nums)+1)] then Use bucket sorting to avoid other types of sorting. With bucket sort we achieve o(n) instead of o(nlog(n))
 
-LeetCode Problem 271 - Encode Decode Strings : Create a simple encode algorithm like # + len + string etc and the decode the value according to the pattern. 
+6 = LeetCode Problem 271 - Encode Decode Strings : Create a simple encode algorithm like # + len + string etc and the decode the value according to the pattern. 
 
 """
