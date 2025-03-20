8a20c4276c0811ff7502b68e9151f8f8af3ecee5
Aligorithm-97 <atemurtas97@gmail.com>
2025-03-20 16:44:37 +0300

    aligorithm-validSudokuBitmask

--- questions/Arrays&Hashing/validSudoku.py
+++ questions/Arrays&Hashing/validSudoku.py
@@ -4,8 +4,8 @@ from typing import List
 """
 
 Solution with Hashset
-time : O(n2)
-space : o(n2)
+time : O(1)
+space : o(1)
 
 """
 
@@ -58,3 +58,43 @@ print(sol.isValidSudoku(board))
 
 print(sol.isValidSudoku(board2))
         
+
+
+
+"""
+
+Solution with Bitmask
+time : O(1)
+space : o(1)
+
+"""
+
+
+class SolutionTwo:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [0] * 9
+        cols = [0] * 9
+        squares = [0] * 9
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                
+                val = int(board[r][c]) - 1
+                if (1 << val) & rows[r]:
+                    return False
+                if (1 << val) & cols[c]:
+                    return False
+                if (1 << val) & squares[(r // 3) * 3 + (c // 3)]:
+                    return False
+                    
+                rows[r] |= (1 << val)
+                cols[c] |= (1 << val)
+                squares[(r // 3) * 3 + (c // 3)] |= (1 << val)
+
+        return True
+    
+
+solTwo = SolutionTwo()
+print(solTwo.isValidSudoku(board))
